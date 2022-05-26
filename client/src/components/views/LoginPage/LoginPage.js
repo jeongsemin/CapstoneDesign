98d08edeb169560254616584ab0906e6adcd21d5
jeongsemin <najsm114@naver.com>
2022-05-26 18:56:28 +0900

    REACT

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,64 @@
+import { Axios } from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage(props) {
+  const navigate = useNavigate();
+  const disaptch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    disaptch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        alert("로그인에 성공하였습니다.");
+        navigate("/");
+      } else {
+        alert("로그인중 오류가 발생했습니다.");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
